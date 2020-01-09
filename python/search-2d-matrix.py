9e199292b13589706c0d34a6d4a9a5909ee9f85a
April <38486418+ricezhang11@users.noreply.github.com>
2020-01-09 11:22:13 -0800

    Update search-2d-matrix.py

--- python/search-2d-matrix.py
+++ python/search-2d-matrix.py
@@ -1,1 +1,27 @@
-
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        # write your code here
+        if len(matrix) == 0:
+            return False
+        elif len(matrix[0]) == 0:
+            return False
+        
+        start = 0
+        m = len(matrix)
+        n = len(matrix[0])
+        end = m * n - 1
+        
+        
+        while start + 1 < end:
+            mid = start + (end - start) / 2
+            if matrix[int(mid / n)][int(mid % n)] < target:
+                start = mid
+            elif matrix[int(mid / n)][int(mid % n)] < target:
+                return True
+            else:
+                end = mid
+                
+        if matrix[int(start / n)][int(start % n)] == target or matrix[int(end / n)]         [int(end % n)] == target:
+            return True
+        else:
+            return False
