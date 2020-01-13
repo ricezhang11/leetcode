6596fbe281158a5b7b213b1b3904eb64d379df33
April <38486418+ricezhang11@users.noreply.github.com>
2020-01-13 11:40:15 -0800

    Update find-minimum-in-a-rotated-array.py

--- python/find-minimum-in-a-rotated-array.py
+++ python/find-minimum-in-a-rotated-array.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return -1
+        
+        start, end = 0, len(nums) - 1
+        while start + 1 < end:
+            mid = int(start + (end - start) / 2)
+            if nums[mid] < nums[end]:
+                end = mid
+            elif nums[mid] > nums[end]:
+                start = mid
+        
+        return min(nums[start], nums[end])
