5644397c3d1c04cd41fc5ac8a95ea8708f74ec98
April <38486418+ricezhang11@users.noreply.github.com>
2020-01-19 13:37:39 -0800

    Update reverse-words-in-a-string-III.py

--- python/reverse-words-in-a-string-III.py
+++ python/reverse-words-in-a-string-III.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        if not s:
+            return s
+        words = s.split()
+        res = ""
+        for j in range(0, len(words)):
+            i = len(words[j]) - 1
+            while i >= 0:
+                res = res + words[j][i]
+                i -= 1
+            if j == len(words) - 1:
+                continue
+            res = res + " "
+                                 
+        return res
