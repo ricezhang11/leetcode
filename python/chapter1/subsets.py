1ecc2aa0a826fe925247cb30b3fe2562ea261c75
April <38486418+ricezhang11@users.noreply.github.com>
2020-01-30 14:47:16 -0800

    Update subsets.py

--- python/chapter1/subsets.py
+++ python/chapter1/subsets.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        combinations = []
+        if nums:  #if nums is not empty
+            self.dfsHelper(nums, 0, [], combinations)
+        return combinations
+        
+    def dfsHelper(self, nums, index, combination, combinations):
+        combinations.append(list(combination))
+        for i in range(index, len(nums)):
+            combination.append(nums[i])
+            self.dfsHelper(nums, i + 1, combination, combinations)
+            combination.pop()  
