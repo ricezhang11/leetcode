b28f5cd9579c3137cf6549dadc0594e4c79baf4a
April <38486418+ricezhang11@users.noreply.github.com>
2020-01-30 14:49:28 -0800

    Update str-str.py

--- python/chapter1/str-str.py
+++ python/chapter1/str-str.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    """
+    @param source: 
+    @param target: 
+    @return: return the index
+    """
+    def strStr(self, source, target):
+        # Write your code here
+        if source == None or target == None:
+            return -1
+        for i in range(len(source) - len(target) + 1):
+            if source[i:i + len(target)] == target:
+                return i
+        return -1
