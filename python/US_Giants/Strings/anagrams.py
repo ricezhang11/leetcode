62cbabc732ec735ceede957c303845b171a76783
April <38486418+ricezhang11@users.noreply.github.com>
2020-01-31 13:08:24 -0800

    Update anagrams.py

--- python/US_Giants/Strings/anagrams.py
+++ python/US_Giants/Strings/anagrams.py
@@ -1,3 +1,23 @@
+# 71. Anagrams
+
+# Given an array of strings, return all groups of strings that are anagrams.
+
+# Example
+# Example 1:
+
+# Input:["lint", "intl", "inlt", "code"]
+# Output:["lint", "inlt", "intl"]
+# Example 2:
+
+# Input:["ab", "ba", "cd", "dc", "e"]
+# Output: ["ab", "ba", "cd", "dc"]
+# Challenge
+# What is Anagram?
+
+# Two strings are anagram if they can be the same after change the order of characters.
+# Notice
+# All inputs will be in lower-case
+
 class Solution:
     """
     @param strs: A list of strings
@@ -20,3 +40,10 @@ class Solution:
                 result = result + item
         
         return result
+
+#sorted('abcd') will return a list of chars" ['a', 'b', 'c', 'd']
+#therefore, we need to join the chars back together using ''.join()
+#the solution is to sort each string and get the sorted string. Strings that are anagrams will have the same sorted string result. 
+#use the sorted string as the key of the dictionary and values are lists of original strings. 
+#to make the results a list of strings. Note that to use d[res] + [st] --- turn st into a list and then do list concatenation. 
+ 
