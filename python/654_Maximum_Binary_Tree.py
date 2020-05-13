e95748ff3edad8d3021abd38682a6bcf3b3245bc
April <38486418+ricezhang11@users.noreply.github.com>
2020-05-13 16:13:39 -0700

    Update 654_Maximum_Binary_Tree.py

--- python/654_Maximum_Binary_Tree.py
+++ python/654_Maximum_Binary_Tree.py
@@ -2,8 +2,8 @@
 # Monotonous stack is great for finding the values in the left and right side in an array
 # that are larger than current value
 
-
 import sys
+
 # Definition for a binary tree node.
 
 
@@ -35,3 +35,44 @@ class Solution:
             if stack:
                 stack[-1].right = node
         return root.right
+    
+    
+# solve by DFS
+
+# Definition for a binary tree node.
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
+        return self.max_helper(nums)
+        
+    def max_helper(self, ls):
+        if not ls:
+            return None
+        if len(ls) == 1:
+            return TreeNode(ls[0])
+        
+        index = self.findMax(ls)
+        node = TreeNode(ls[index])
+        
+        left = self.max_helper(ls[:index])
+        right = self.max_helper(ls[index + 1:])
+        
+        node.left = left
+        node.right = right
+        
+        return node
+        
+    def findMax(self, ls):
+        m = (0, ls[0])
+        
+        for i, v in enumerate(ls):
+            if v > m[1]:
+                m = (i, v)
+        return m[0]
